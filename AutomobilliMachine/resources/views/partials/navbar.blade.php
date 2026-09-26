9bd4dc7f4b4868521d40e3e6c261d8ef7de46f9a
Enggar Bayu Pratama <enggarbayuprtma@gmail.com>
2026-09-26 12:32:30 +0800

    Make shared navigation work across pages

--- AutomobilliMachine/resources/views/partials/navbar.blade.php
+++ AutomobilliMachine/resources/views/partials/navbar.blade.php
@@ -1,19 +1,32 @@
 <nav class="navbar navbar-expand-lg navbar-dark sticky-top navbar-custom py-3">
     <div class="container">
-        <a class="navbar-brand brand-font fw-bold" href="{{ url('/') }}">
+        <a class="navbar-brand brand-font fw-bold" href="{{ route('home') }}">
             <i class="fa-solid fa-gauge-high text-danger me-2"></i>AUTOMOBILLI
         </a>
+
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMenu">
             <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse" id="navMenu">
             <ul class="navbar-nav mx-auto mb-2 mb-lg-0 gap-3">
-                <li class="nav-item"><a class="nav-link active" href="{{ url('/') }}">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="#brands">Brands</a></li>
-                <li class="nav-item"><a class="nav-link" href="#iconic">Iconic Cars</a></li>
-                <li class="nav-item"><a class="nav-link" href="#compare">Compare</a></li>
-                <li class="nav-item"><a class="nav-link" href="#membership">Membership</a></li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="{{ route('home') }}">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/#brands') }}">Brands</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/#iconic') }}">Iconic Cars</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('compare.index') }}">Compare</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/#membership') }}">Membership</a>
+                </li>
             </ul>
+
             <div class="d-flex gap-2">
                 <a href="#" class="btn btn-outline-custom btn-sm px-3">Sign In</a>
                 <a href="#" class="btn btn-racing btn-sm px-3">Register</a>
